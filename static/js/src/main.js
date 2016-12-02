ae4ed4ba59c36275b887356458b0f6fad3f6bc33
Brian McFee <brian.mcfee@nyu.edu>
2016-12-02 16:13:32 -0500

    renamed urbanears to annotator

--- static/js/src/main.js
+++ static/js/src/main.js
@@ -13,7 +13,7 @@
  *       magma // color scheme array that maps 0 - 255 to rgb values
  *    
  */
-function UrbanEars() {
+function Annotator() {
     this.wavesurfer;
     this.playBar;
     this.stages;
@@ -73,7 +73,7 @@ function UrbanEars() {
     this.addEvents();
 }
 
-UrbanEars.prototype = {
+Annotator.prototype = {
     addWaveSurferEvents: function() {
         var my = this;
 
@@ -242,8 +242,8 @@ UrbanEars.prototype = {
 
 function main() {
     // Create all the components
-    var urbanEars = new UrbanEars();
+    var annotator = new Annotator();
     // Load the first audio annotation task
-    urbanEars.loadNextTask();
+    annotator.loadNextTask();
 }
 main();
